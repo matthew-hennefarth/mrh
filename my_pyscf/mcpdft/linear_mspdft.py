d9cb5cf18278616806e23ee3c698424f6ad5c4f2
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2022-10-07 14:30:39 -0500

    temp again

--- my_pyscf/mcpdft/linear_mspdft.py
+++ my_pyscf/mcpdft/linear_mspdft.py
@@ -4,36 +4,49 @@ from functools import reduce
 import numpy as np
 
 from pyscf import ao2mo
+from mrh.my_pyscf.mcpdft import _dms
 
-def h_for_cas(mc, mo_coeff=None, ncas=None, ncore=None):
+# def h_for_cas(mc, mo_coeff=None, ncas=None, ncore=None):
+    # if mo_coeff is None: mo_coeff = mc.mo_coeff
+    # if ncas is None: ncas = mc.ncas
+    # if ncore is None: ncore = mc.ncore
+
+    # mo_core = mo_coeff[:,:ncore]
+    # mo_cas = mo_coeff[:,ncore:ncore+ncas]
+
+    # hcore = mc.get_hcore()
+    
+    # core_dm = np.dot(mo_core, mo_core.conj().T) * 2
+    # energy_core = np.einsum('ij,ji', core_dm, hcore).real
+
+    # hcas = reduce(np.dot, (mo_cas.conj().T, hcore, mo_cas))
+
+    # h = reduce(np.dot, (mo_coeff.conj().T, hcore, mo_coeff))
+
+    # # Should return h_tu (hcas) and 2 sum_i h_ii
+    # # interms of active-orbitals
+    # return hcas, energy_core
+
+def get_h(mc, mo_coeff=None):
     if mo_coeff is None: mo_coeff = mc.mo_coeff
-    if ncas is None: ncas = mc.ncas
-    if ncore is None: ncore = mc.ncore
 
-    mo_core = mo_coeff[:,:ncore]
-    mo_cas = mo_coeff[:,ncore:ncore+ncas]
-
-    hcore = mc.get_hcore()
+    return reduce(np.dot, (mo_coeff.conj().T, mc.get_hcore(), mo_coeff))
     
-    core_dm = np.dot(mo_core, mo_core.conj().T) * 2
-    energy_core = np.einsum('ij,ji', core_dm, hcore).real
-    
-    hcas = reduce(np.dot, (mo_cas.conj().T, hcore, mo_cas))
 
-    # Should return h_tu (hcas) and 2 sum_i h_ii
-    # interms of active-orbitals
-    return hcas, energy_core
-
-def eris_for_cas(mc, mo_coeff=None, ncas=None):
+def get_g(mc, mo_coeff=None):
     if mo_coeff is None: mo_coeff = mc.mo_coeff
-    if ncas is None: ncas = mc.ncas
 
-    eri = ao2mo.full(mc._eri, mo_coeff)
-    
+    return ao2mo.restore(1, ao2mo.get_ao_eri(mc.mol), mo_coeff.shape[0])
 
-    print(aeri)
+def get_D0(mc, ci=None):
+    if ci is None:
+        ci = mc.ci
 
-def get_linear_pdft_ham(mc, mo_coeff=None, ci=None, ncas=None, ncore=None):
+    nstates = len(ci)
+    for root in nstates:
+        print(root)
+
+def get_linear_pdft_ham(mc, mo_coeff=None, ci=None, ncas=None, ncore=None, nelecas=None):
     if mo_coeff is None:
         mo_coeff = mc.mo_coeff
 
@@ -46,12 +59,19 @@ def get_linear_pdft_ham(mc, mo_coeff=None, ci=None, ncas=None, ncore=None):
     if ncore is None:
         ncore = mc.ncore
 
-    hcas, energy_core = h_for_cas(mc, mo_coeff=mo_coeff, ncas=ncas, ncore=ncore)
-
-    eris_for_cas(mc, mo_coeff=mo_coeff, ncas=ncas)
+    # the h_pq in MO basis
+    hmo = get_h(mc, mo_coeff=mo_coeff)
     
+    # the d_pqrs in MO basis
+    gmo = get_g(mc, mo_coeff=mo_coeff)
 
+    fcisolver, _, nelecas = _dms._get_fcisolver(mc, ci)
 
+    #
+    #casdm1_transit, casdm2_transit = fcisolver.trans_rdm12(ci[1], ci[0], ncas, nelecas)
+    #print(casdm1_transit)
+    #print(casdm2_transit.shape)
+    get_D0(mc)
 
 if __name__ == "__main__":
     from pyscf import gto, scf, mcscf
