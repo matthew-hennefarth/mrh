2af8da563b8394e82109104a73508a5c600fda06
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2022-10-26 15:32:16 -0500

    cleaned up h1eff func

--- my_pyscf/mcpdft/linear_mspdft.py
+++ my_pyscf/mcpdft/linear_mspdft.py
@@ -43,22 +43,23 @@ def weighted_average_densities(mc, ci=None, weights=None, ncas=None):
     return tuple(casdm1s_0), casdm2_0
 
 
-def get_effhconst(mc, Eot_0, veff1_0, veff2_0, casdm1s_0, casdm2_0, mo_coeff=None, ncas=None, ncore=None):
+def get_effhconst(mc, veff1_0, veff2_0, casdm1s_0, casdm2_0, mo_coeff=None, ot=None, ncas=None, ncore=None):
     # This should be correct now where we are
     # returning the h_const term in my derivations
     if mo_coeff is None: mo_coeff = mc.mo_coeff
+    if ot is None: ot = mc.otfnal
     if ncas is None: ncas = mc.ncas
     if ncore is None: ncore = mc.ncore
-
     nocc = ncore + ncas
 
-    mo_core = mo_coeff[:, :ncore]
-    mo_cas = mo_coeff[:, ncore:nocc]
-    # Active space Density matrix for the expansion term
+    # Get the 1-RDM matrices
     casdm1_0 = casdm1s_0[0] + casdm1s_0[1]
-    
     dm1s = _dms.casdm1s_to_dm1s(mc, casdm1s=casdm1s_0)
     dm1 = dm1s[0] + dm1s[1]
+   
+    #Eot for zeroth order state
+    Eot_0 = mc.energy_dft(ot=ot, mo_coeff=mo_coeff, casdm1s=casdm1s_0,
+                          casdm2=casdm2_0)
     
     #Coulomb energy for zeroth order state
     vj = mc._scf.get_j(dm=dm1)
@@ -71,21 +72,17 @@ def get_effhconst(mc, Eot_0, veff1_0, veff2_0, casdm1s_0, casdm2_0, mo_coeff=Non
     E_veff2 = veff2_0.energy_core
     E_veff2 += np.tensordot(veff2_0.vhf_c[ncore:nocc, ncore:nocc], casdm1_0)
     E_veff2 += np.tensordot(mc.get_h2eff_lin(veff2_0), casdm2_0, axes=4)
-    
+   
     # h_nuc + Eot - 1/2 g_pqrs D_pq D_rs - V_pq D_pq - v_pqrs d_pqrs
     energy_core = mc.energy_nuc() + Eot_0 - E_j - E_veff1 - E_veff2
     return energy_core
 
-
-def transformed_h1e_for_cas(mc, Eot_0, veff1_0, veff2_0, casdm1s_0, casdm2_0,
-                            mo_coeff=None, ncas=None, ncore=None):
+def transformed_h1e_for_cas(mc, veff1_0, veff2_0, casdm1s_0, casdm2_0,
+                            mo_coeff=None, ncas=None, ncore=None, ot=None):
     '''CAS Space one-electron Linear PDFT Hamiltonian
 
     Args:
         mc : instance of a _PDFT object
-
-        Eot_0 : float
-            On-top energy for the expansion density.
 
         veff1_0 : ndarray with shape (nao, nao)
             1-body effective potential in the AO basis.
@@ -98,6 +95,10 @@ def transformed_h1e_for_cas(mc, Eot_0, veff1_0, veff2_0, casdm1s_0, casdm2_0,
 
         casdm1s_0 : ndarray of shape (2,ncas,ncas)
             Spin-separated 1-RDM in the active space generated
+            from expansion density
+
+        casdm2_0 : ndarray of shape (ncas,ncas,ncas,ncas)
+            Spin-summed 2-RDM in the active space generated
             from expansion density
 
         mo_coeff : ndarray of shape (nao,nmo)
@@ -117,6 +118,7 @@ def transformed_h1e_for_cas(mc, Eot_0, veff1_0, veff2_0, casdm1s_0, casdm2_0,
     if mo_coeff is None: mo_coeff = mc.mo_coeff
     if ncas is None: ncas = mc.ncas
     if ncore is None: ncore = mc.ncore
+    if ot is None: ot=mc.otfnal
 
     nocc = ncore + ncas
     mo_core = mo_coeff[:, :ncore]
@@ -125,32 +127,25 @@ def transformed_h1e_for_cas(mc, Eot_0, veff1_0, veff2_0, casdm1s_0, casdm2_0,
     # Active space Density matrix for the expansion term
     casdm1_0 = casdm1s_0[0] + casdm1s_0[1]
 
-    hcore_eff = mc.get_hcore() + veff1_0
-    energy_core = mc.get_effhconst(Eot_0, veff1_0, veff2_0, casdm1s_0,
-                               casdm2_0)
-
-    # Gets the 2 electron integrals in MO
-    eris = ao2mo.restore(1, ao2mo.get_mo_eri(mc.mol, mo_coeff), len(mo_coeff))
-    # Partition for summations
-    eris_core = eris[:, :, :ncore, :ncore]
-    eris_cas = eris[:, :, ncore:nocc, ncore:nocc]
-
-    # The 2\sum_j g_pqjj + g_pqvw D_vw^0 term (all in MO basis)
-    reduced_coulomb = 2 * np.einsum("pqjj->pq", eris_core) + np.einsum(
-        "pqvw,vw->pq", eris_cas, casdm1_0)
+    dm1s = _dms.casdm1s_to_dm1s(mc, casdm1s=casdm1s_0)
+    dm1 = dm1s[0] + dm1s[1]
+    v_j = mc._scf.get_j(dm=dm1)
+   
+    # h_pq + V_pq + J_pq all in AO integrals
+    hcore_eff = mc.get_hcore() + veff1_0 + v_j
+    energy_core = mc.get_effhconst(veff1_0, veff2_0, casdm1s_0,
+                               casdm2_0, ot=ot)
 
     if mo_core.size != 0:
         core_dm = np.dot(mo_core, mo_core.conj().T) * 2
         energy_core += veff2_0.energy_core
         energy_core += np.einsum('ij,ji', core_dm, hcore_eff).real
-        energy_core += 2 * np.trace(reduced_coulomb[:ncore, :ncore])
 
     h1eff = reduce(np.dot,
                    (mo_cas.conj().T, hcore_eff, mo_cas)) + veff2_0.vhf_c[
                                                            ncore:nocc,
                                                            ncore:nocc]
 
-    h1eff += reduced_coulomb[ncore:nocc, ncore:nocc]
     return h1eff, energy_core
 
 
@@ -171,19 +166,18 @@ def make_heff_lin(mc, mo_coeff=None, ci=None, ot=None):
     ncas = mc.ncas
     casdm1s_0, casdm2_0 = mc.get_casdm12_0()
 
-    Eot_0 = mc.energy_dft(ot=ot, mo_coeff=mo_coeff, casdm1s=casdm1s_0,
-                          casdm2=casdm2_0)
     veff1_0, veff2_0 = mc.get_pdft_veff(mo=mo_coeff, casdm1s=casdm1s_0,
                                         casdm2=casdm2_0)
 
-    h1, h0 = mc.get_h1eff_lin(Eot_0, veff1_0, veff2_0, casdm1s_0, casdm2_0)
+    h1, h0 = mc.get_h1eff_lin(veff1_0, veff2_0, casdm1s_0, casdm2_0, ot=ot)
     h2 = mc.get_h2eff_lin(veff2_0)
     h2eff = direct_spin1.absorb_h1e(h1, h2, ncas, mc.nelecas, 0.5)
     hc_all = [direct_spin1.contract_2e(h2eff, c, ncas, mc.nelecas) for c in ci]
     heff = np.tensordot(ci, hc_all, axes=((1, 2), (1, 2)))
     idx = np.diag_indices_from(heff)
     heff[idx] += h0
-    
+   
+    print(heff)
     return heff
 
 
@@ -257,7 +251,7 @@ if __name__ == "__main__":
 
     mol = gto.M(atom='''Li 0 0 0
                         H 1.5 0 0''',
-                basis='6-31G**',
+                basis='STO-3G',
                 verbose=1,
                 spin=0)
     mf = scf.RHF(mol).run()
